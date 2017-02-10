1f9c3425842650557227c495d35310add6c1b167
lucianferrel <lucian.ferrel@mail.com>
2017-02-10 15:04:09 +0100

    .

--- angular/app.routing.js
+++ angular/app.routing.js
@@ -5,7 +5,7 @@
         .module('myFirstApp')
         .config(config);
 
-    function config(($routeProvider) {
+    function config($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'angular/home/home.view.html',
