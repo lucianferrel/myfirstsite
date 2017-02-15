02d3534208df7814471b57cd3cb6c9f5fe054e3f
lucianferrel <lucian.ferrel@mail.com>
2017-02-15 13:14:42 +0100

    .

--- angular/home/home.controller.js
+++ angular/home/home.controller.js
@@ -10,14 +10,18 @@
         var vm = this;
         vm.capital = '70000';
         vm.revenu = '13000';
+        vm.reval = '400';
         vm.interest = '3';
         vm.period = '0';
         vm.newCapital = '70000';
+        vm.newRevenu = '13000';
 
         vm.evaluate = function() {
             vm.newCapital = vm.capital;
+            vm.newRevenu = vm.revenu;
             for (var i = 0; i < Number(vm.period); i++) {
                 vm.newCapital = (Number(vm.newCapital))*(1+Number(vm.interest)*0.01) + Number(vm.revenu);
+                vm.newRevenu = vm.revenu + vm.reval;
             }
         };
 
